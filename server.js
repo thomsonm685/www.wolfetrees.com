43e2e8053935b0dc417ce19adec394625a6389ab
Michael Thomson <michael@takoagency.com>
2023-04-14 21:04:50 -0500

    server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(__filename); 
 
 const app = express();
 
